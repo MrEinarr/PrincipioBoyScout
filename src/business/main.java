cdad544ef6dcf8c72444ecd0ada144088e21cbf6
MrEinarr <dutex14@gmail.com>
2024-03-05 12:39:20 -0600

    xd

--- src/business/main.java
+++ src/business/main.java
@@ -3,11 +3,8 @@ package business;
 public class main {
 
 	public static void main(String[] args) {
-		
 		Controller control = new Controller();
-		control.getControl();
-		
-		
+		control.startController();
 	}
 
 }
--- src/presentation/GUI.java
+++ src/presentation/GUI.java
@@ -6,23 +6,32 @@ public class GUI {
 
 	public GUI() {
 	}
-	
-	public int showInput(String message) {
-		
-		int num = Integer.parseInt(JOptionPane.showInputDialog(message));
-		return num;
-	};
-	
-	
-	public String getProduct(String message) {
-		return message;
-	};
 
-	public void showInformation(String message) {
-		JOptionPane.showMessageDialog(null, message, "", JOptionPane.INFORMATION_MESSAGE);
+	public static int menu00() {
+		int option = JOptionPane.showConfirmDialog(null, "¡Bienvenido al Sistema de Compra!\n¿Vas a comprar?", "Bienvenida al Sistema de Compra", JOptionPane.YES_NO_OPTION);
+		return option;
 	}
-	
-	public static void showError(String message) {
-		JOptionPane.showMessageDialog(null, message, "Critico", JOptionPane.ERROR_MESSAGE);
+
+	public static int menu01(){
+		int number = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de productos que desea comprar."));
+		return number;
 	}
+
+	public static int menu(){
+		int option = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero del producto que deasea comprar.\n1 .leche\n2 .Galletas\n3 .Botana\n4 .Arroz\n5 .Refresco\n6 .Frijoles"));
+		return option;
+	}
+
+	public static void showShoppingList(String shoppingList) {
+		JOptionPane.showMessageDialog(null, shoppingList, "Lista de Compra", JOptionPane.PLAIN_MESSAGE);
+	}
+
+	public static void showAddList() {
+		JOptionPane.showMessageDialog(null, "Producto Agregado con exito", "ShopCar", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public static void showErrorDialog() {
+        JOptionPane.showMessageDialog(null, "Ocurrio Un Error", "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
 }
