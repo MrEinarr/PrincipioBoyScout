6625c8fb0ca664772fc2f4b21bc79d7236ac645d
MrEinarr <dutex14@gmail.com>
2024-03-05 18:05:57 -0600

    xd

--- src/business/Controller.java
+++ src/business/Controller.java
@@ -9,24 +9,24 @@ public class Controller {
 	public void startController() {
 		boolean flag = true;
 		while (flag) {
-			int option = GUI.menu00();//inicia el rpogarama o lo termina
+			int option = GUI.menu00();
 
 			if (option == JOptionPane.YES_OPTION) {
 				int n = GUI.menu01(); // Determina el tamaño de la lista
-				shopCar shopcar = new shopCar(n);// Crea el objeto carrito de compra
+				shopCar shopcar = new shopCar(n);
 
 				for (int i = 0; i < n; i++) { // Iterar sobre el número de productos (n)
-					int productOption = GUI.menu(); // Solicitar al usuario que elija un producto
-					if (shopcar.addProduct(productOption)) { // Añadir el producto al carrito de compras
-						GUI.showAddList();// Producto agregado
+					int productOption = GUI.menu();
+					if (shopcar.addProduct(productOption)) {
+						GUI.showAddList();
 					} else {
-						GUI.showErrorDialog();// Error al agregar
+						GUI.showErrorDialog();
 					}
 				}
-				String shoppingList = shopcar.toString(); // Obtener la lista de compra como una cadena
-				GUI.showShoppingList(shoppingList); // Pasar la cadena al GUI para mostrarla
+				String shoppingList = shopcar.toString();
+				GUI.showShoppingList(shoppingList);
 
-			} else if (option == JOptionPane.NO_OPTION || option == JOptionPane.CLOSED_OPTION) { //Cerrar programa
+			} else if (option == JOptionPane.NO_OPTION || option == JOptionPane.CLOSED_OPTION) {
 				flag = false;
 			}
 		}
